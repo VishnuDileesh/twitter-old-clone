5ad1ecff23e6bb31f137c0544e82048f72ff8dc9
Vishnu Dileesh <dev@vishnudileesh.com>
2020-09-21 19:40:33 +0530

    Add - login with email and password functionality

--- src/components/LoginFormComponent/index.js
+++ src/components/LoginFormComponent/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import styles from './index.module.css';
 
+import { auth } from '../../services/firebase';
+
 const LoginFormComponent = () => {
+
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    
+    auth.signInWithEmailAndPassword(email, password)
+      .catch((error) => console.log(error))
+  };
+
+
   return (
     <div>
       <h3>Please sign in</h3>
@@ -10,13 +33,13 @@ const LoginFormComponent = () => {
       <form className={styles.form}>
         <div className={styles.formField}>
           <label htmlFor="email">email address</label>
-          <input className={styles.inputField} type="email" id="email"/>
+          <input className={styles.inputField} value={email} onChange={handleEmailChange} type="email" id="email"/>
         </div>
         <div className={styles.formField}>
           <label htmlFor="password">password</label>
-          <input className={styles.inputField} type="password" id="password"/>
+          <input className={styles.inputField} value={password} onChange={handlePasswordChange} type="password" id="password"/>
         </div>
-        <button className="btn">Sign in</button>
+        <button className="btn" onClick={formSubmit}>Sign in</button>
       </form>
     </div>
   );
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,16 +1,16 @@
 import firebase from 'firebase';
 
 const config = {
-  appKey: process.env.REACT_APP_API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_Database_URL,
-  projectId: process.env.REACT_APP_Project_ID,
-  storageBucket: process.env.REACT_APP_Storage_Bucket,
-  messagingSenderId: process.env.REACT_APP_Messaging_Sender_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(config);
 
-export const auth = firebase.auth;
+export const auth = firebase.auth();
 export const db = firebase.firestore();
